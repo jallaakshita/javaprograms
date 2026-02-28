5da46618eebddf4825f8910f415015439f77157f
jallaakshita <jallaakshita24@gmail.com>
2026-02-28 12:29:29 +0530

    Add files via upload

---
+++ username.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class username
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String name;
+System.out.println("enter name");
+name=sc.next();
+System.out.println("welcome  "+name);
+}
+}
+
+
