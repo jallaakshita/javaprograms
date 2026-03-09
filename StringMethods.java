1bac47a94b696b13c7aa25501ff98e2aead4e785
jallaakshita <jallaakshita24@gmail.com>
2026-03-09 20:33:18 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,13 @@
+class StringMethods
+{
+	public static void main(String args[])
+	{
+		String str="Hello Akshita";
+		System.out.println(" uppercase :"+str.toUpperCase());
+		System.out.println(" lowercase :"+str.toLowerCase());
+		System.out.println(" length :"+str.length());
+		System.out.println(" concatination :"+str.concat("! Welcome"));
+		System.out.println("position:"+str.charAt(0));
+		System.out.println("index:"+str.indexOf(' '));
+	}
+}
