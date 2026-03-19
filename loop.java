47df4286189b6a00a9ed8f10bd58f2186e0fd5f3
jallaakshita <jallaakshita24@gmail.com>
2026-03-19 22:48:45 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class loop
+{
+public static void main(String args[])
+{
+int i;
+int j=1,k=1;
+for(i=1;i<=5;i++)
+{
+System.out.println(i);
+}
+
+while(j<=5)
+{
+System.out.println(j);
+j++;
+}
+   
+do
+{
+System.out.println(k);
+k++;
+}while(k<=5);
+
+}
+}
+
